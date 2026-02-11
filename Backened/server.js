39973441709b2c5bd90f1fe4a1091558fd1dbd1b
Umashankar12345 <umashankarkumar9572@gmail.com>
2026-02-11 21:46:19 +0530

    daily practice

---
+++ Backened/server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+
+const app = express();
+// const PORT = 5000;
+
+
+app.use(express.json());
+
+
+app.get("/", (req, res) => {
+    res.send("API is running...");
+});
+
+
+app.get("/users", (req, res) => {
+    res.json([
+        { id: 1, name: "Uma" },
+        { id: 2, name: "Rahul" }
+    ]);
+});
+
+
+app.post("/users", (req, res) => {
+    const newUser = req.body;
+    res.json({
+        message: "User created successfully",
+        user: newUser
+    });
+});
+
+
+app.listen(5000, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
